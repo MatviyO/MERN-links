1c440b8b8cde18ee1f708aac5fe234ea34c314cf
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-08 22:58:43 +0300

    context

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
-import 'materialize-css'
 import {useRoutes} from "./routes";
+import {useAuth} from "./hooks/auth.hook";
+import 'materialize-css'
+import {AuthContext} from "./context/Auth.context";
 
 function App() {
+    const {token, logout, login, userId} = useAuth()
     const routes = useRoutes(false)
     return (
-        <Router>
-            <div className="container">
-                {routes}
-            </div>
-        </Router>
+        <AuthContext>
+            <Router>
+                <div className="container">
+                    {routes}
+                </div>
+            </Router>
+        </AuthContext>
     );
 }
 
